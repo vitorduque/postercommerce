d3346ef2fe6eb188efc438b94b03defc58c47e33
vitorduque <vitor.duque@rivendel.com.br>
2016-05-15 20:54:02 -0300

    some fixes

--- app/models/graph_dao.rb
+++ app/models/graph_dao.rb
@@ -52,7 +52,12 @@ class GraphDao
     return items, orders_id
   end
 
-  def retrieve_graph_two
+  def retrieve_graph_two(graph)
+    year = ""
+    year << graph.begin[0]
+    year << graph.begin[1]
+    year << graph.begin[2]
+    year << graph.begin[3]
     db = @conn.open
     male = Array.new
     female = Array.new
@@ -60,7 +65,7 @@ class GraphDao
     #male buys
     result = db.query("select DATE_FORMAT(date, '%m') as order_date, count(*) as count
                         from orders
-                        join clients on orders.clients_id = clients.id where clients.gender = 'Male'
+                        join clients on orders.clients_id = clients.id where clients.gender = 'Male' AND date like '#{year}%'
                         group by order_date")
 
     result.each do |row|
@@ -73,7 +78,7 @@ class GraphDao
     #female buys
     result = db.query("select DATE_FORMAT(date, '%m') as order_date, count(*) as count
                         from orders
-                        join clients on orders.clients_id = clients.id where clients.gender = 'Female'
+                        join clients on orders.clients_id = clients.id where clients.gender = 'Female' AND date like '#{year}%'
                         group by order_date")
 
     result.each do |row|
@@ -91,7 +96,7 @@ class GraphDao
     if option.eql? 1
       retrieve_graph_one(date)
     elsif option.eql? 2
-      retrieve_graph_two
+      retrieve_graph_two(date)
     end
   end
 
